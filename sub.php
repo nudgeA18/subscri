33dc00ad0f975f807d7d948073133cca9ea520f5
nudgeA18 <86913604+nudgeA18@users.noreply.github.com>
2021-08-16 16:45:55 +0530

    Add files via upload

---
+++ sub.php
@@ -0,0 +1,16 @@
+<?php
+<div class="container">
+		<h1 class="text-center m-5">GET GITHUB TIMELINE UPDATES ON YOUR MAIL!</h1>
+		
+	</div>
+	<section class="bg-dark text-center p-5 mt-4">
+		<div class="container p-3">
+			<h3 class="text-white">SUBSCRIBE NOW</h3>
+			<form action="#" method="Post">
+				<input type="text" name="text" placeholder="Enter Your Email Id">
+
+				<button type="button" class="btn btn-default">Subscribe<i class="fa fa-envelope"></i></button>
+			</form>
+		</div>
+	</section>
+?>
